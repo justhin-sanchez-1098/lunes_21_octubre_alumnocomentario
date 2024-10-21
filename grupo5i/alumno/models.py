a8726c9b7b239ae3dbbc667fc26b7b5035de64fc
justhin-sanchez-1098 <167554940+justhin-sanchez-1098@users.noreply.github.com>
2024-10-21 12:28:23 -0600

    Add files via upload

---
+++ grupo5i/alumno/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Alumno(models.Model):
+    apaterno= models.CharField(max_length=50, verbose_name="Apellido Paterno")
+    amaterno= models.CharField(max_length=50, verbose_name="Apellido Materno")
+    nombre=models.CharField(max_length=100, verbose_name="Nombre (s)")
+    fecha_nacimiento= models.DateField(verbose_name="Fecha de nacimiento", null=False, blank=False)
+    fecha_ingreso= models.DateField(verbose_name="Fecha de ingreso", null=False, blank=False)
+
+    class Meta:
+        db_table="Alumnos"
+        verbose_name="Alumno"
+        verbose_name_plural="Alumnos"
+    
+    def __str__(self) -> str:
+        return self.nombre
+
+class Frase(models.Model):
+    comentario= models.TextField(verbose_name="Comentario", max_length=400)
+    Alumno_fk= models.ForeignKey(Alumno, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name="Frase"
+        verbose_name_plural="Frases"
